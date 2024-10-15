73527149245087eac2c24fade450b28360088eab
Nassim <jslegend@protonmail.com>
2024-10-15 15:00:57 -0400

    added graphics  + character controller

--- src/initGame.js
+++ src/initGame.js
@@ -1,17 +1,41 @@
 import initKaplay from "./kaplayCtx";
 
-const k = initKaplay();
-
 export default function initGame() {
   const DIAGONAL_FACTOR = 1 / Math.sqrt(2);
+  const k = initKaplay();
+
+  k.loadSprite("background", "./background.png");
+  k.loadSprite("characters", "./characters.png", {
+    sliceY: 2,
+    sliceX: 8,
+    anims: {
+      "down-idle": 0,
+      "up-idle": 1,
+      "right-idle": 2,
+      "left-idle": 3,
+      right: { from: 4, to: 5, loop: true },
+      left: { from: 6, to: 7, loop: true },
+      down: { from: 8, to: 9, loop: true },
+      up: { from: 10, to: 11, loop: true },
+      "npc-down": 12,
+      "npc-up": 13,
+      "npc-right": 14,
+      "npc-left": 15,
+    },
+  });
+
+  k.add([k.sprite("background"), k.pos(0, -70), k.scale(8)]);
 
   const player = k.add([
-    k.rect(16, 16),
+    k.sprite("characters", { anim: "down-idle" }),
+    k.area(),
+    k.body(),
     k.anchor("center"),
     k.pos(k.center()),
+    k.scale(8),
     "player",
     {
-      speed: 128,
+      speed: 800,
     },
   ]);
 
@@ -25,6 +49,38 @@ export default function initGame() {
     if (k.isKeyDown("up")) movementVector.y = -1;
     if (k.isKeyDown("down")) movementVector.y = 1;
 
+    if (
+      movementVector.eq(k.vec2(-1, 0)) &&
+      player.getCurAnim().name !== "left"
+    ) {
+      player.play("left");
+    }
+
+    if (
+      movementVector.eq(k.vec2(1, 0)) &&
+      player.getCurAnim().name !== "right"
+    ) {
+      player.play("right");
+    }
+
+    if (movementVector.eq(k.vec2(0, -1)) && player.getCurAnim().name !== "up") {
+      player.play("up");
+    }
+
+    if (
+      movementVector.eq(k.vec2(0, 1)) &&
+      player.getCurAnim().name !== "down"
+    ) {
+      player.play("down");
+    }
+
+    if (
+      movementVector.eq(k.vec2(0, 0)) &&
+      !player.getCurAnim().name.includes("idle")
+    ) {
+      player.play(`${player.getCurAnim().name}-idle`);
+    }
+
     if (movementVector.x && movementVector.y) {
       player.move(movementVector.scale(DIAGONAL_FACTOR * player.speed));
       return;
@@ -32,4 +88,13 @@ export default function initGame() {
 
     player.move(movementVector.scale(player.speed));
   });
+
+  const npc = k.add([
+    k.sprite("characters", { anim: "npc-left" }),
+    k.area(),
+    k.body({ isStatic: true }),
+    k.anchor("center"),
+    k.scale(8),
+    k.pos(1480, 500),
+  ]);
 }
--- src/kaplayCtx.js
+++ src/kaplayCtx.js
@@ -2,8 +2,8 @@ import kaplay from "kaplay";
 
 export default function initKaplay() {
   return kaplay({
-    width: 256,
-    height: 144,
+    width: 1920,
+    height: 1080,
     letterbox: true,
     global: false,
     debug: true, // put back to false in prod
